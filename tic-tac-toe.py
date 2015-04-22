db99094372a943ff0a35af57329e84248c59eb75
Kara McNair <github@karamcnair.com>
2015-04-21 19:13:03 -0700

    Working algorithm for computer play.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -5,6 +5,7 @@ class Board(object):
     """
     squares = [' ',' ',' ', ' ',' ',' ', ' ',' ',' ']
     i = 8
+    won = False
 
     # some helper variables for identifying each possible winning trio.
     row = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
@@ -28,7 +29,6 @@ class Board(object):
 
     def empties(self):
         indexes = [i for i, x in enumerate(self.squares) if x == ' ']
-        print 'empties = ' + `indexes`
         return indexes
         #todo - use in computer play selection
 
@@ -44,43 +44,37 @@ class Board(object):
 
     # need to be able to take an array of 3 values & make sure they're a winning 'row'
     def win(self, trio_values):
-        print "called win-1 with " + `trio_values`
         if ' ' in trio_values:
             return False
         return all (x == trio_values[0] for x in trio_values)
 
-    # get the board values associated with those 3 indexes 
-    def get_trio(self, squares, trio_indexes):
-        print "called get_trio-2"
-        return [squares[x] for x in trio_indexes]
 
-    # this function checks a specific board (squares) for a win 
-    def wins_game(self, squares, trio_indexes):
-        print "called wins_game-3"
+    # this function checks a specific trio on a specific board (squares) for a win 
+    def trio_wins(self, squares, trio_indexes):
         trio_values = [squares[x] for x in trio_indexes]
-        print "trio values " + `trio_values`
         return self.win(trio_values)
 
     # this function checks if ANY wins arise from playing 'play_index' being played
     # on the current board. We need it anyway because we need to test the human entries
-    def won(self, play_index):
-        print "called won-4"
+    def wins_game(self, squares, play_index):
         play_row = play_index / 3
         play_col = play_index % 3
-        print "row " + `play_row` + ", col " + `play_col`
 
         # probably GET the rows here instead so 
 
-        if self.wins_game(self.squares, Board.row[play_row]):
+        if self.trio_wins(squares, Board.row[play_row]):
             return True
-        elif self.wins_game(self.squares, Board.col[play_col]):
+        elif self.trio_wins(squares, Board.col[play_col]):
             return True
         elif play_index in Board.upDiag:
-            if self.wins_game(self.squares, Board.upDiag):
+            if self.trio_wins(squares, Board.upDiag):
                 return True
             elif play_index in Board.downDiag:
-                return self.wins_game(self.squares, Board.downDiag)
+                return self.trio_wins(squares, Board.downDiag)
         return False
+
+    def won(self, play_index):
+        return self.wins_game(self.squares, play_index)
 
 
 class Player(object):
@@ -98,25 +92,24 @@ class Player(object):
         if self.human:
             # get input from keyboard to play
             empties = board.empties()
-            play_index = empties[0]
+            play_index = empties[len(empties)-1]
+            print "Human playing "+ self.my_token + " in location " + `play_index`
+
 
         else:
             play_index = self.computer_play(board)
+            print "Computer playing "+ self.my_token + " in location " + `play_index`
 
         # this is the part that ties the 'wins' check to having actually played
         # need to figure out/refactor 'wins' with a 'candidate' trio instead.
 
         board.squares[play_index] = self.my_token
-        print 'player.token = ' + self.my_token
         return play_index
 
     def computer_play(self, board):
 
-        print "in computer_play with board " + `board.squares`
-
         # where CAN I play?
         empties = board.empties()
-        print "computer empties = " + `empties`
 
         oppo_wins = []
         # this would be to find any possible 'better' spaces b/c I already have one lined up
@@ -132,21 +125,21 @@ class Player(object):
             temp_squares[play_index] = self.my_token
 
             # the first winning position is my choice b/c it ends the game
-            if Board.wins_game(board.squares):
+            if board.wins_game(temp_squares, play_index):
                 return play_index
             else:
                 temp_squares[play_index] = self.oppo_token
                 # keep track of opponent wins to block!
-                if Board.wins_game(board.squares):
+                if board.wins_game(temp_squares, play_index):
                     oppo_wins.append(play_index)
                 else:
                     # this is kinda bogus b/c it might be one with an opponent but I don't want to drill into that yet
                     my_partials.append(play_index)
  
         # if my opponent has ANY win plays, I have to block. (If more than one, I lose anyway)
-        if len(oppo_wins) > 1:
+        if len(oppo_wins) > 0:
             return oppo_wins[0]
-        if len(my_partials) > 1:
+        if len(my_partials) > 0:
             return my_partials[0]
         return empties[0]           
 
@@ -154,7 +147,7 @@ class Player(object):
 board = Board()
 
 # set up players (any humans?)
-player1 = Player("X", "O")
+player1 = Player("X", "O", True)
 player2 = Player("O", "X")
 
 # show the initial board
@@ -164,18 +157,22 @@ currentPlayer = player1
 winner = None
 
 while board.hasEmpties() and winner is None:
-  print "======="
-  print "======="
+    print "======= + Player " + currentPlayer.my_token + " is a ",
+    if currentPlayer.human:
+        print "human" 
+    else:
+        print "computer"
+        print "======="
 
-  play = currentPlayer.play(board)
-  board.display()
+    playIndex = currentPlayer.play(board)
+    board.display()
 
-  if board.won(play):
-    winner = currentPlayer
-    print "Congrats to Player " + currentPlayer.my_token + "!" 
-  elif board.hasEmpties() == False:
-    print "Congrats to the cat. Meow! " 
-  else:
-    currentPlayer = player2 if currentPlayer == player1 else player1
+    if board.won(playIndex):
+        winner = currentPlayer
+        print "Congrats to Player " + currentPlayer.my_token + "!" 
+    elif board.hasEmpties() == False:
+        print "Congrats to the cat. Meow! " 
+    else:
+        currentPlayer = player2 if currentPlayer == player1 else player1
 
 
