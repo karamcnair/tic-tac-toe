4b19347b87d660dcf9a7a9d00df403a11d7cec37
Kara McNair <github@karamcnair.com>
2015-05-16 17:32:55 -0700

    No idea. This is from a different machine so I am really not understanding what's going on with git here.

--- board.py
+++ board.py
@@ -3,8 +3,6 @@ class Board(object):
     """ It's easier to deal with list operations on a 1D
     So instead I'll 'mock' the indexes for the columns & rows.
     """
-    squares = [' ',' ',' ', ' ',' ',' ', ' ',' ',' ']
-    won = False
 
     # some helper variables for identifying each possible winning trio.
     row = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
@@ -13,6 +11,7 @@ class Board(object):
     upDiag = [6, 4, 2]
 
     def __init__(self):
+        self.squares = [' ',' ',' ', ' ',' ',' ', ' ',' ',' ']
         self.won = False
 
     def display(self):
